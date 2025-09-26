70933f0d1ec16c45e4dfd39633e32119795f0484
Aisha Abdullahi <ana9@calvin.edu>
2025-09-26 19:29:12 -0400

    doneeeee

--- todo-app/app/index.tsx
+++ todo-app/app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import TodoItem from '../components/todo-item';
 import { Stack } from 'expo-router';
 
